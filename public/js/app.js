0d3fb92d9ce0e7cc856bda0f2c8840d29ee9eb53
kolawole olaniyi <kolaniyi3@gmail.com>
2023-03-15 19:52:35 +0100

    implemented front to query backend and display converted text

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,54 @@
 const $translateForm = document.querySelector("form");
-const $textInput = document.querySelector("#textareaInput");
-const $targetLanguageInput = document.querySelector("#targetLanguageInput");
+const $fileInput = document.querySelector("#file-input");
 const submitBtn = document.querySelector("#btn-submit");
-const outPutParagraph = document.querySelector(".translated-text");
+const outPutParagraph = document.querySelector(".converted-text");
+const imageContainer = document.querySelector(".uploaded-img__container");
+
+let uploadedImg = null;
+
+$fileInput.addEventListener("change", function () {
+  const file = this.files[0];
+  const reader = new FileReader();
+  uploadedImg = file;
+
+  reader.addEventListener("load", function () {
+    const imageDataUrl = reader.result;
+    let existingImage = imageContainer.querySelector("img");
+    if (existingImage) {
+      imageContainer.removeChild(existingImage);
+    }
+    const image = new Image();
+    image.src = imageDataUrl;
+    image.classList.add("img-fluid");
+    imageContainer.appendChild(image);
+  });
+
+  reader.readAsDataURL(file);
+});
 
 $translateForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const text = $textInput.value;
-  const target = $targetLanguageInput.value;
-
   submitBtn.textContent = "Loading...";
+  submitBtn.disabled = true;
 
-  const body = {
-    text,
-    target,
-  };
+  const formData = new FormData($translateForm);
 
-  fetch("/api/v1/translate", {
+  fetch("/api/v1/read", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
+    body: formData,
   })
     .then((response) => {
       return response.json().then((data) => {
-        console.log(data);
-        submitBtn.textContent = "Submit";
-        outPutParagraph.textContent = data.translation;
+        submitBtn.textContent = "Convert";
+        outPutParagraph.textContent = data.text;
+        submitBtn.disabled = false;
       });
     })
     .catch((err) => {
       console.log(err);
-      alert("Error translating that text...");
-      submitBtn.textContent = "Submit";
+      alert("Error converting that image...");
+      submitBtn.textContent = "Convert";
+      submitBtn.disabled = false;
     });
 });
